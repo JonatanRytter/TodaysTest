b19ba1e09bd36138244b1e3a9d94d520a0019a8c
32monkeys <38853524+32monkeys@users.noreply.github.com>
2019-10-09 13:02:09 +0200

    createSuper method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // purple is a fruit
         System.out.println("Wauv");
-    }
 
+    }
+    public void createSuperpowers(){
+        System.out.println("I AM MIGHTY!!!");
+
+    }
 }
